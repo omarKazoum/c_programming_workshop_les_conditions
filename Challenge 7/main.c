2766f253e20f269464bd3d42cee971c58db8d10e
omar kazoum <omarkazoum96@gmail.com>
2021-11-15 09:40:57 +0100

    ajouter le support de l'année bissextile

--- Challenge 7/main.c
+++ Challenge 7/main.c
@@ -13,11 +13,18 @@ Pour plus de simplicité, n'incluez pas l'année bissextile (sauf si vous êtes cou
 1 an = 365 jours 1 mois = 30 jours
 */
 int main(){
-    
+    printf("entrez une année\n");
+	int year;
+	scanf("%d",&year);
     int mois,jours,heurs,minutes,seconds;
-    mois=12;
-    jours=365;
-    heurs=jours*24;
+	mois=12;
+    
+	if(year%4==0){
+      jours=366;
+	}else{
+	   jours=365;
+	}
+	heurs=jours*24;
     minutes=heurs*60;
     seconds=minutes*60;
     printf("Bonjour saviez vous que une annee contient: %d mois\n%djours  \n%dheurs \n%dminutes \n%dseconds",mois,jours,heurs,minutes,seconds);
